db89bad0cf76286a03600d2c98a0ef64a23718ca
mitravajjala <69940532+mitravajjala@users.noreply.github.com>
2022-02-15 17:12:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 // TODO: Create this function
 async function getPokemon() {
     const pokemon = retrieveInputs();
+    /**https://pokeapi.co/api/v2/ability/{id or name}/**/
+    for(let i = 0; i < pokemon.length; i++){
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+pokemon[i])
+        const obj = await response.json()
+        const types = obj.types
+        var hp = obj.stats[0].base_stat
+        var attack = obj.stats[1].base_stat
+        var defense = obj.stats[2].base_stat
+        var special_attack = obj.stats[3].base_stat
+        var special_defence = obj.stats[4].base_stat
+        var height = obj.height
+        var weight = obj.weight
+        var image_url = obj.sprites.front_default
+        var speed = obj.stats[5].base_stat
+        const abilities = obj.abilities
+        buildCard(pokemon[0], types, hp, attack, defense, special_attack, special_defence, speed, abilities, height, weight, image_url)
+    }
+    
 }
 
 /**
